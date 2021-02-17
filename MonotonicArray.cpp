01a565eb92dcf962c5dab99bd024067117dce8f5
Gabriel Araujo <51542095+gabrujo@users.noreply.github.com>
2021-02-16 21:24:00 -0500

    Create MonotonicArray.cpp

---
+++ MonotonicArray.cpp
@@ -0,0 +1,9 @@
+    bool isMonotonic(vector<int> A){
+        bool inc=true, dec=true;
+        for (int i=1;i<A.size();i++){
+            inc &= A[i-1] <= A[i]; // inc = inc & (A[i-1] <= A[i])
+            dec &= A[i-1] >= A[i];
+        }
+        return inc || dec;
+        
+    }  
